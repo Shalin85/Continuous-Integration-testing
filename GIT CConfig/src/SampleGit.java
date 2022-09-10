5e593435ffaf5788f464812130fc493cacc3346d
Shalin85 <shalin2385@gmail.com>
2022-09-10 19:53:12 +0530

    Java source file

---
+++ GIT CConfig/src/SampleGit.java
@@ -0,0 +1,12 @@
+
+public class SampleGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int a = 9;
+ int b = 2;
+ int c=a+b;
+ System.out.println(c);
+	}
+
+}
